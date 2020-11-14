9818ec25769516b5b25c514b0654dcaa94b3de19
guro <guramidgebuadze@gmail.com>
2020-11-14 16:26:00 +0400

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
+import Input from '../src/components/form/Input/input';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <main>
+        <form action="">
+          <button type="submit" onClick={(e) => submit(e)}>Submit</button>
+        </form>
+      </main>
+    </React.Fragment>
   );
 }
 
 export default App;
+
