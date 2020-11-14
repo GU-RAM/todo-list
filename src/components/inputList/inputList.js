14ebd7426cf8d01b9370f30934f750be10baceb3
guro <guramidgebuadze@gmail.com>
2020-11-14 17:33:35 +0400

    write logic for adding and removing todoes in list

--- src/components/inputList/inputList.js
+++ src/components/inputList/inputList.js
@@ -1,9 +1,27 @@
 import React from 'react'
+// import styled from 'styled-components';
 
-export default function inputList() {
+export default function inputList({ todo, todoList, deleteTodo, setTodoHandler }) {
+
+    const list = () => {
+        return (
+            <ul>
+                {todoList && todoList.map((list, index) => {
+                    return (
+                        list && list.id && list.listTodo && <li key={list.id}>Text {index + 1} <span>{list.listTodo}</span> <span onClick={() => deleteTodo(list.id)}>X</span></li>
+                    )
+                })
+                }
+            </ul>
+        )
+    }
+
     return (
-        <div>
-
-        </div>
+        <>
+            <div>
+                {list()}
+            </div>
+            <input type="text" placeholder="Enter Todo" value={todo} onChange={(e) => setTodoHandler(e)} />
+        </>
     )
 }
--- src/containers/todoList/TodoList.js
+++ src/containers/todoList/TodoList.js
@@ -1,17 +1,47 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import InputList from '../../components/inputList/inputList';
-import './App.css';
 
 function TodoList() {
+    const [todo, setTodo] = useState('');
+    console.log("TodoList -> todo", todo)
+    const [todoList, setTodoList] = useState([]);
+    const [listId, setListId] = useState(1);
+    const [numberOfTodoes, setNumberOfTodoes] = useState(0);
 
+
+    useEffect(() => {
+        setNumberOfTodoes(todoList.length);
+    }, [todoList]);
+
+    const setTodoHandler = (e) => {
+        e.target.value && setTodo(e.target.value);
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        todo && addListHandler({ listTodo: todo });
+    }
+
+    const addListHandler = (list) => {
+        setTodoList((prevTodo) => [...prevTodo, { id: listId, ...list }]);
+        setListId(listId + 1);
+        clearTodoHandler();
+    }
+
+    const clearTodoHandler = (e) => {
+        setTodo('');
+    }
+
+    const deleteTodo = (listToDelete) => {
+        setTodoList(() => todoList.filter(item => item.id !== listToDelete));
+    }
 
     return (
         <React.Fragment>
             <main>
                 <form action="">
-                    <h1>You Have Todos</h1>
-                    <InputList />
-                    <input type="text" />
+                    <h1>You Have {numberOfTodoes} Todos</h1>
+                    <InputList todo={todo} todoList={todoList} deleteTodo={deleteTodo} setTodoHandler={setTodoHandler} />
                     <button type="submit" onClick={(e) => submit(e)}>Submit</button>
                 </form>
             </main>
